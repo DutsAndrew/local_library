1ef5e0cd6e5c54324a6c2ebbc6583ca8bea2e97d
Andrew Dutson <Dutsandrew@gmail.com>
2023-02-11 21:08:46 -0700

    genre, author, book form hooked up to controllers

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -1,7 +1,7 @@
 const Author = require('../models/author');
-
 const async = require("async");
 const Book = require("../models/book");
+const { body, validationResult } = require("express-validator");
 
 // display list of all authors
 exports.author_list = (req, res, next) => {
@@ -44,14 +44,53 @@ exports.author_detail = (req, res, next) => {
 };
 
 // display author create form on GET
-exports.author_create_get = (req, res) => {
-  res.send('NOT IMPLEMENTED: Author create GET');
+exports.author_create_get = (req, res, next) => {
+  res.render("author_form", { title: "Create Author" });
 };
 
 // handles author create on POST
-exports.author_create_post = (req, res) => {
-  res.send('NOT IMPLEMENTED: Author create POST');
-};
+exports.author_create_post = [
+  body("first_name")
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage("First name must be specified.")
+    .isAlphanumeric()
+    .withMessage("First name has non-alphanumeric characters."),
+  body("family_name")
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage("Family name must be specified.")
+    .isAlphanumeric()
+    .withMessage("Family name has non-alphanumeric characters."),
+  body("date_of_death", "Invalid date of Death")
+    .optional({ checkFalsy: true })
+    .isISO8601()
+    .toDate(),
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.render("author_form", {
+        title: "Create Author",
+        author: req.body,
+        errors: errors.array(),
+      });
+      return;
+    };
+
+    const author = new Author({
+      first_name: req.body.first_name,
+      family_name: req.body.family_name,
+      date_of_birth: req.body.date_of_birth,
+      date_of_death: req.body.date_of_death,
+    });
+    author.save((err) => {
+      if (err) return next(err);
+      res.redirect(author.url);
+    });
+  },
+];
 
 // handles author delete form on GET
 exports.author_delete_get = (req, res) => {
